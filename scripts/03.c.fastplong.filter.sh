4cd723ebc8cd57f4143404e16a2aa845fa2e6ab9
mgeaghan <michael.p.geaghan@gmail.com>
2026-07-20 17:10:49 +1000

    update output path for second filtlong script again

--- scripts/03.c.fastplong.filter.sh
+++ scripts/03.c.fastplong.filter.sh
@@ -8,11 +8,11 @@ FASTQ="${1}"
 ID=$(basename ${FASTQ} .fastq)
 
 mkdir -p trim_filter
-mkdir -p qc/fastplong_filter
+mkdir -p qc/fastplong
 
 fastplong \
     -i ${FASTQ} \
     -q 13 \
     -o trim_filter/${ID}.trimmed.filtered.fastq \
-    -h qc/fastplong_filter/${ID}.fastplong_report.html \
-    -j qc/fastplong_filter/${ID}.fastplong_report.json
+    -h qc/fastplong/${ID}.fastplong_filter_report.html \
+    -j qc/fastplong/${ID}.fastplong_filter_report.json
