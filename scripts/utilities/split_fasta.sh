befca344237e9d133435ef310fd53cd6a8415438
mgeaghan <michael.p.geaghan@gmail.com>
2026-08-14 12:16:41 +1000

    update split_fasta

--- scripts/utilities/split_fasta.sh
+++ scripts/utilities/split_fasta.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+shopt -s expand_aliases
 module load seqkit
 
 ASSEMBLY="${1}"
 ID="${2}"
 
-mkdir -p assembly/split/${ID}
+mkdir -p assembly/${ID}/split/
 
-seqkit split -i "${ASSEMBLY}" --by-id-prefix assembly/split/${ID}
+seqkit split -i "${ASSEMBLY}" --by-id-prefix assembly/${ID}/split/
